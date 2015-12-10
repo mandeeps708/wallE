e03cdbb41e6956cd72b18fc8d1390af653ff7152
mandeeps708 <mandeeps708@gmail.com>
2015-12-10 23:54:36 +0530

    auto-change wallpaper

---
+++ TODO.md
@@ -0,0 +1,5 @@
+* Auto wallpaper select after specified time (default 1 minute)
+* Works for both gnome and non-gnome (openbox atleast) shells.
+* What if feh is not installed?
+* Make an installer script for creating the main script. That will include
+  the prompt for time (or probably the directory).
--- wallE.sh
+++ wallE.sh
@@ -2,16 +2,18 @@
 path=$PWD
 #echo $path
 base="$path"
-photo=$(find $base -regex ".*jpg"  -type f | while read x; do echo "`expr $RANDOM % 1000`:$x"; done | sort -n | sed 's/[0-9]*://' | head -1)
+while true; do
+    photo=$(find $base -regex ".*jpg"  -type f | while read x; do echo "`expr $RANDOM % 1000`:$x"; done | sort -n | sed 's/[0-9]*://' | head -1)
+    session=`echo $DESKTOP_SESSION`
+    path="file://"$photo
 
-session=`echo $DESKTOP_SESSION`
-path="file://"$photo
+    sleep 60
+    # for openbox.
+    if [[ $session == *"openbox"* ]]; then
+        feh --bg-fill $photo
 
-# for openbox.
-if [[ $session == *"openbox"* ]]; then
-    feh --bg-fill $photo
-
-# for Ubuntu and Gnome.
-elif [[ $session == *"ubuntu"* ]]; then
-    gsettings set org.gnome.desktop.background picture-uri "$path"
-fi
+    # for Ubuntu and Gnome.
+    elif [[ $session == *"ubuntu"* ]]; then
+        gsettings set org.gnome.desktop.background picture-uri "$path"
+    fi
+done
